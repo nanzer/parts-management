18756f07b8a862e97a1973bb29f4c55e146cfef7
Abhay <abhay@abhayraj.net>
2019-10-07 19:30:45 -0400

    Added various components
    - Added NavBar
    - Added Register page
    - Added proper App.vue
    - Added .env files (local and online)
    - Fixed icons by importing material design

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import * as firebase from 'firebase'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 
 Vue.config.productionTip = false
+Vue.use(vuetify)
 
 new Vue({
   router,
@@ -12,3 +17,14 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+var config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+}
+firebase.initializeApp(config)
---
+++ src/views/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+    <!-- display the navigation bar -->
+    <v-toolbar>
+      <v-toolbar-items>
+        <v-btn
+          text
+            tag='span'
+            style='cursor: pointer'
+            >
+          Parts Management
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+<!-- navigation bar links -->
+      <v-toolbar-items class='hidden-xs-only'>
+        <v-btn text v-for="item in items" :key="item.title" :to="item.link">
+          <v-icon left>{{item.icon}}</v-icon>{{item.title}}
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-toolbar-items>
+      <!-- sign out button -->
+      <v-toolbar-items class='hidden-xs-only'>
+        <v-btn
+          text
+          >
+          <v-icon left>delete_sweep</v-icon>Logout
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      let menuItems =  [
+        {
+          title: 'Register',
+          icon: 'face',
+          link: '/register'
+        },
+        {
+          title: 'Login',
+          icon: 'no_encryption',
+          link: '/login'
+        }
+      ]
+    return menuItems
+    }
+  }
+}
+</script>
