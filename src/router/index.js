68b42a431ba997fa2239b626e82e706b405ecf81
Abhay <abhay@abhayraj.net>
2019-10-07 19:32:06 -0400

    Added Login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
 import Register from '@/components/auth/Register.vue'
+import Login from '@/components/auth/Login.vue'
+
 
 Vue.use(Router)
 
@@ -15,6 +17,11 @@ export default new Router({
       path: '/register',
       name: 'Register',
       component: Register
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
     // {
     //   path: '/',
