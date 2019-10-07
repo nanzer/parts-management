aa1e4b214f3891efe6bc5d48b3d4a9be3bcf6cd7
Abhayraj Jain <903424+nanzer@users.noreply.github.com>
2019-10-07 12:18:07 -0400

    Fix Vuetify Attempt #3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <v-app id="inspire">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <core-toolbar v-if="hasToolbar" />
   </v-app>
 </template>
 
 <script>
+  // Mixins
+  import Meta from '@/mixins/meta'
+  // Utilities
+  import { waitForReadystate } from '@/util/helpers'
+  import { mapState } from 'vuex'
+  import languages from '@/data/i18n/languages.json'
+  const fallbackLocale = languages.find(lang => lang.fallback === true).locale
   export default {
-    props: {
-      source: String,
-    },
+    mixins: [Meta],
     data: () => ({
-      drawer: null,
+      availableLocales: languages.map(lang => lang.alternate || lang.locale),
+      languages,
     }),
+    computed: {
+      ...mapState('app', ['isLoading']),
+      ...mapState('route', ['hash', 'name']),
+      languageIsValid () {
+        return this.availableLocales.includes(this.$route.params.lang)
+      },
+      hasToolbar () {
+        return this.languageIsValid && this.name !== 'Layouts'
+      },
+    },
+    created () {
+      if (!this.languageIsValid) this.$router.push(`/${fallbackLocale}`)
+    },
+    async mounted () {
+      if (!this.hash) return
+      await this.$nextTick()
+      await waitForReadystate()
+      this.$vuetify.goTo(this.hash)
+    },
   }
 </script>
+
+<style>
+  .text-decoration-none {
+    text-decoration: none;
+  }
+  .wf-loading .material-icons {
+    display: none;
+  }
+</style>
