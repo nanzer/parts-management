a7f69f3adff0cfb6424fc8922276219690856d10
Abhayraj Jain <903424+nanzer@users.noreply.github.com>
2019-10-07 11:40:24 -0400

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,84 @@
 <template>
-  <div class="login">
-    <img src="../assets/logo.png" width="300px" />
-    <h3>DC Comics Rebirth - Covers</h3>
-    <input type="text" v-model="email" placeholder="Email address" class="input" required />
-    <br />
-    <input type="password" v-model="password" placeholder="Password" class="input" required />
-    <br />
-    <button v-on:click="login" class="button">Enter</button>
-    <p>
-      <router-link to="/signup">New Here? Create a new account</router-link>
-    </p>
-  </div>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <div class="flex-grow-1"></div>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      href="https://codepen.io/johnjleider/pen/pMvGQO"
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-codepen</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    v-model="email"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    v-model="password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
 </template>
 
 <script src="https://apis.google.com/js/platform.js" async defer></script>
